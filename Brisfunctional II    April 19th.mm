ccb514c0437d20275fa74c874835d148d414a64b
Matthew Gilliard <matthew.gilliard@gmail.com>
2011-04-19 19:10:03 +0100

    Added mindmap

---
+++ Brisfunctional II    April 19th.mm
@@ -0,0 +1,195 @@
+<map version="0.9.0">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node COLOR="#000000" CREATED="1303205076260" ID="ID_1992569046" MODIFIED="1303220899460" STYLE="fork">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p style="text-align: center">
+      Brisfunctional II
+    </p>
+    <p style="text-align: center">
+      April 19th
+    </p>
+  </body>
+</html>
+</richcontent>
+<font NAME="SansSerif" SIZE="20"/>
+<hook NAME="accessories/plugins/AutomaticLayout.properties"/>
+<node COLOR="#0033ff" CREATED="1303205632227" ID="ID_610895906" MODIFIED="1303220241795" POSITION="left" TEXT="Network stuff">
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+<font NAME="SansSerif" SIZE="18"/>
+<node COLOR="#00b439" CREATED="1303205647634" ID="ID_138838448" MODIFIED="1303220241797" TEXT="WiFi">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+<node COLOR="#990000" CREATED="1303223015447" ID="ID_1638013002" MODIFIED="1303223024074" TEXT="http//guest.nokia.com:950">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+</node>
+<node COLOR="#00b439" CREATED="1303205654697" ID="ID_441485380" MODIFIED="1303220241798" TEXT="Github">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+<node COLOR="#990000" CREATED="1303223025327" ID="ID_929481445" MODIFIED="1303223033746" TEXT="http://github.com/brisfunctional">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+<node COLOR="#990000" CREATED="1303223035054" ID="ID_1808144667" MODIFIED="1303223046345" TEXT="checkout the tdd-projects repo">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+</node>
+<node COLOR="#00b439" CREATED="1303205657922" ID="ID_1228631133" MODIFIED="1303220241799" TEXT="Google Groups">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+<node COLOR="#990000" CREATED="1303223048143" ID="ID_1100519153" MODIFIED="1303224697439" TEXT="http://groups.google.com/group/brisfunctional">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+</node>
+<node COLOR="#00b439" CREATED="1303205661993" ID="ID_1139832271" MODIFIED="1303220241800" TEXT="Twitter">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+<node COLOR="#990000" CREATED="1303224699420" ID="ID_1906873151" MODIFIED="1303224710039" TEXT="@BrisFunctional - #brisfunctional">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+</node>
+</node>
+<node COLOR="#0033ff" CREATED="1303205695049" ID="ID_1609368611" MODIFIED="1303220241803" POSITION="right">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      This evening
+    </p>
+  </body>
+</html></richcontent>
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+<font NAME="SansSerif" SIZE="18"/>
+<node COLOR="#00b439" CREATED="1303205860869" FOLDED="true" ID="ID_390754332" MODIFIED="1303225849277" TEXT="Theme">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+<node COLOR="#990000" CREATED="1303205864516" ID="ID_190980092" MODIFIED="1303220241806">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Testing &amp; TDD in
+    </p>
+    <p>
+      Functional Programming
+    </p>
+  </body>
+</html></richcontent>
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+</node>
+<node COLOR="#00b439" CREATED="1303205880580" ID="ID_1234853877" MODIFIED="1303220241810" TEXT="Problem">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+<node COLOR="#990000" CREATED="1303205884443" ID="ID_1608245520" MODIFIED="1303225850704" TEXT="L-Systems">
+<font NAME="SansSerif" SIZE="14"/>
+<node COLOR="#111111" CREATED="1303219259825" ID="ID_710932633" MODIFIED="1303225853696">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      A thing that can transform a string into another
+    </p>
+    <p>
+      according to a fixed set of rules, applied char-by-char
+    </p>
+  </body>
+</html></richcontent>
+<node COLOR="#111111" CREATED="1303220003541" HGAP="68" ID="ID_1574180734" MODIFIED="1303221742598" TEXT="eg.  String &quot;A&quot; and rule &quot;A-&gt;AB&quot; gives AB" VSHIFT="-1"/>
+<node COLOR="#111111" CREATED="1303220026669" HGAP="69" ID="ID_1106361384" MODIFIED="1303221746486" TEXT="Apply twice &amp; get AAB - 3 times for AAAB" VSHIFT="6"/>
+<node COLOR="#111111" CREATED="1303220452745" HGAP="55" ID="ID_1860138502" MODIFIED="1303221755493" TEXT="Apply as many times as you like :)  Infinitely recursive" VSHIFT="10"/>
+</node>
+<node COLOR="#111111" CREATED="1303220103355" HGAP="64" ID="ID_955056886" MODIFIED="1303221764845" VSHIFT="4">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Originally used to model growth of algae &amp; plants
+    </p>
+    <p>
+      - also generates fractals
+    </p>
+  </body>
+</html></richcontent>
+</node>
+<node COLOR="#111111" CREATED="1303220807160" ID="ID_167825504" MODIFIED="1303220812372" TEXT="(look on wikipedia)"/>
+</node>
+<node COLOR="#990000" CREATED="1303220245376" HGAP="35" ID="ID_1805313333" MODIFIED="1303221808921" TEXT="Testing" VSHIFT="75">
+<font NAME="SansSerif" SIZE="14"/>
+<node COLOR="#111111" CREATED="1303220773041" ID="ID_1259028144" MODIFIED="1303220802508" TEXT="What are inputs &amp; outputs of the L-system?  Test those!"/>
+<node COLOR="#111111" CREATED="1303220815625" FOLDED="true" HGAP="33" ID="ID_1891461813" MODIFIED="1303222601122" TEXT="example tests 1" VSHIFT="32">
+<node COLOR="#111111" CREATED="1303220819768" ID="ID_215393244" MODIFIED="1303221523466">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      <font color="rgb(0, 0, 0)" face="arial, sans-serif" size="3">starting string A, rules (), &#160;&#160;&#160;&#160;&#160;&#160;&#160;one iteration, output is A<br style="line-height: normal; word-spacing: 0px; text-transform: none; font-variant: normal; text-indent: 0px; letter-spacing: normal; white-space: normal" size="3" align="-webkit-auto" />starting string A, rules (A-&gt;A), &#160;&#160;&#160;one iteration, output is A<br style="line-height: normal; word-spacing: 0px; text-transform: none; font-variant: normal; text-indent: 0px; letter-spacing: normal; white-space: normal" size="3" align="-webkit-auto" />starting string A, rules (A-&gt;AA), &#160;&#160;one iteration, output is AA<br style="line-height: normal; word-spacing: 0px; text-transform: none; font-variant: normal; text-indent: 0px; letter-spacing: normal; white-space: normal" size="3" align="-webkit-auto" />starting string A, rules (A-&gt;B), &#160;&#160;&#160;one iteration, output is B<br style="line-height: normal; word-spacing: 0px; text-transform: none; font-variant: normal; text-indent: 0px; letter-spacing: normal; white-space: normal" size="3" align="-webkit-auto" />starting string A, rules (A-&gt;ABCD), one iteration, output is ABCD</font>
+    </p>
+  </body>
+</html></richcontent>
+</node>
+</node>
+<node COLOR="#111111" CREATED="1303220843016" FOLDED="true" HGAP="23" ID="ID_1469804807" MODIFIED="1303222600890" TEXT="example tests 2" VSHIFT="2">
+<node COLOR="#111111" CREATED="1303221442008" HGAP="31" ID="ID_151428012" MODIFIED="1303221831243" VSHIFT="36">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      <font color="rgb(0, 0, 0)" face="arial, sans-serif" size="3">starting string A, rules (), &#160;&#160;&#160;&#160;&#160;&#160;&#160;2 iterations, output is A<br style="line-height: normal; word-spacing: 0px; text-transform: none; font-variant: normal; text-indent: 0px; letter-spacing: normal; white-space: normal" size="3" align="-webkit-auto" />starting string A, rules (A-&gt;A), &#160;&#160;&#160;2 iterations, output is A<br style="line-height: normal; word-spacing: 0px; text-transform: none; font-variant: normal; text-indent: 0px; letter-spacing: normal; white-space: normal" size="3" align="-webkit-auto" />starting string A, rules (A-&gt;AA), &#160;&#160;2 iterations, output is AAAA<br style="line-height: normal; word-spacing: 0px; text-transform: none; font-variant: normal; text-indent: 0px; letter-spacing: normal; white-space: normal" size="3" align="-webkit-auto" />starting string A, rules (A-&gt;B), &#160;&#160;&#160;2 iterations, output is B<br style="line-height: normal; word-spacing: 0px; text-transform: none; font-variant: normal; text-indent: 0px; letter-spacing: normal; white-space: normal" size="3" align="-webkit-auto" />starting string A, rules (A-&gt;ABCD), 2 iterations, output is ABCDBCD<br style="line-height: normal; word-spacing: 0px; text-transform: none; font-variant: normal; text-indent: 0px; letter-spacing: normal; white-space: normal" size="3" align="-webkit-auto" /><br style="line-height: normal; word-spacing: 0px; text-transform: none; font-variant: normal; text-indent: 0px; letter-spacing: normal; white-space: normal" size="3" align="-webkit-auto" />starting string A, rules (A-&gt;AB, B-&gt;C), &#160;2 iterations, output is ABC</font>
+    </p>
+  </body>
+</html></richcontent>
+</node>
+</node>
+<node COLOR="#111111" CREATED="1303220861535" FOLDED="true" ID="ID_889562662" MODIFIED="1303222600586" TEXT="example tests 3">
+<node COLOR="#111111" CREATED="1303220865295" HGAP="12" ID="ID_1446713431" MODIFIED="1303221828291" TEXT="starting string A, rules (A-&gt;B, B-&gt;AB),  1 iterations, output is B&#xa;starting string A, rules (A-&gt;B, B-&gt;AB),  2 iterations, output is AB&#xa;starting string A, rules (A-&gt;B, B-&gt;AB),  3 iterations, output is BAB&#xa;starting string A, rules (A-&gt;B, B-&gt;AB),  4 iterations, output is ABBAB&#xa;starting string A, rules (A-&gt;B, B-&gt;AB),  5 iterations, output is BABABBAB&#xa;starting string A, rules (A-&gt;B, B-&gt;AB),  6 iterations, output is ABBABBABABBAB&#xa;starting string A, rules (A-&gt;B, B-&gt;AB),  7 iterations, output is&#xa;BABABBABABBABBABABBAB&#xa; // count the lengths of these strings - recognise a pattern? :)" VSHIFT="41"/>
+</node>
+</node>
+<node COLOR="#990000" CREATED="1303225671082" HGAP="15" ID="ID_1150912306" MODIFIED="1303225823004" TEXT="Part 2" VSHIFT="30">
+<font NAME="SansSerif" SIZE="14"/>
+<node COLOR="#111111" CREATED="1303225677557" ID="ID_554593848" MODIFIED="1303225681926" TEXT="Turtle Graphics"/>
+<node COLOR="#111111" CREATED="1303225682674" ID="ID_1710661828" MODIFIED="1303225711819">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      one last transform to turn each
+    </p>
+    <p>
+      letter to a turtle command (or to an empty string)
+    </p>
+  </body>
+</html></richcontent>
+</node>
+<node COLOR="#111111" CREATED="1303225712841" ID="ID_88802554" MODIFIED="1303225722340" TEXT="Turtle commands">
+<node COLOR="#111111" CREATED="1303225722689" ID="ID_1452365609" MODIFIED="1303225725036" TEXT="fd 100"/>
+<node COLOR="#111111" CREATED="1303225725529" ID="ID_1269908225" MODIFIED="1303225727620" TEXT="rt 90"/>
+<node COLOR="#111111" CREATED="1303225727824" ID="ID_309353065" MODIFIED="1303225729412" TEXT="lt 90"/>
+<node COLOR="#111111" CREATED="1303225730177" ID="ID_244150742" MODIFIED="1303225734348" TEXT="bk 100"/>
+<node COLOR="#111111" CREATED="1303225735377" ID="ID_956048650" MODIFIED="1303225806698" TEXT="setpc &quot;red">
+<node COLOR="#111111" CREATED="1303225896301" ID="ID_1012850587" MODIFIED="1303225905231" TEXT="or use html-stype: setpc &quot;#f88"/>
+</node>
+</node>
+<node COLOR="#111111" CREATED="1303228846096" ID="ID_1483631672" MODIFIED="1303228854499" TEXT="http://www.amberfrog.com/logo/"/>
+</node>
+</node>
+</node>
+</node>
+</map>
